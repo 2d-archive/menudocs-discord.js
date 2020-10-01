f8b26d7e963be92dee2f654f93996e7c94ed3578
LolWastedJS <ml2dofficial@gmail.com>
2020-10-01 07:33:22 -0700

    ✨  set hello command ratelimit

--- src/Commands/Hello.js
+++ src/Commands/Hello.js
@@ -4,12 +4,17 @@ module.exports = class extends Command {
 
 	constructor(...args) {
 		super(...args, {
-			aliases: ['hallo']
+			aliases: ['hallo'],
+			ratelimit: {
+				bucket: 2,
+				// seven seconds
+				reset: 7 * 1000,
+				stack: false
+			}
 		});
 	}
 
-	// eslint-disable-next-line no-unused-vars
-	async run(message, args) {
+	async run(message) {
 		message.channel.send('Hello!');
 	}
 
